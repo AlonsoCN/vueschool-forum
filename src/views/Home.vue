51fdc3838e6f85a7ef52bd1e7f9b3431f89cf442
Luis Alonso Calle <luis.alonso.cn@gmail.com>
2019-02-05 01:04:47 -0500

    Structure refactor; cleanup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="col-full">
+    <h1>Welcome to the forum</h1>
+    <ThreadList :threads="threads"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import sourceData from '@/data.json';
+import ThreadList from '../components/ThreadList.vue';
+
+console.info(sourceData.threads);
 
 export default {
-  name: 'home',
   components: {
-    HelloWorld,
+    ThreadList,
+  },
+  data() {
+    return {
+      threads: Object.values(sourceData.threads),
+      posts: sourceData.posts,
+      users: sourceData.users,
+    };
   },
 };
 </script>
